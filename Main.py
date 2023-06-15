d2b3f7755e1d93bdeb9507b351afade7436d0a8c
Sianuga <dzikiewicz.pawel@gmail.com>
2023-06-16 00:40:08 +0200

    Port inclusion

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import subprocess
 import virtualbox 
 import time
 
@@ -43,8 +42,11 @@ def create_www_server(session, session2):
     wantHTML = input("Do you want to create HTML configuration? (Y/N): ")
     folderName = input("Give folder name: ")
     port = input("Give port number (default: 80): ")
+    print("You need to change DocumentRoot to /var/www/Imie_Nazwisko/html/ in 000-default.conf file (Imie_Nazwisko is an example)")
+    print("You need to change <Directory /var/www/> to <Directory /var/www/Imie_Nazwisko/html/> in apache2.conf file (Imie_Nazwisko is an example)")
+    print("You need to change <VirtualHost *:80> to <VirtualHost *:port> in 000-default.conf file (port is a number you gave, default is 80, IT NEEDS TO BE THE SAME EVERYWHERE)")
+    print("You need to change Listen 80 to Listen port in ports.conf file (port is a number you gave, default is 80, IT NEEDS TO BE THE SAME EVERYWHERE)")
     if wantHTML == "Y":
-     
         session2.console.keyboard.put_keys("sudo apt update"+ 
                                       "; sudo apt install links"+
                                         "; sudo apt install apache2"+
@@ -55,11 +57,39 @@ def create_www_server(session, session2):
                                         f"; sudo chmod -R 755 /var/www/{folderName}"+
                                         "; sudo nano /etc/apache2/sites-available/000-default.conf"+
                                         "; sudo nano /etc/apache2/apache2.conf"+
-                                        "; sudo service restart apache2"+
-                                        "; links 10.1.2.102"+
+                                        "; sudo nano /etc/apache2/ports.conf"+
+                                        "; sudo systemctl restart apache2"+
+                                        f"; links 10.1.2.102:{port}"+
                                       f'\n')
+        
+        session.console.keyboard.put_keys("sudo apt update"+
+                                        "; sudo apt install links"+
+                                        "; sudo apt install apache2"+
+                                        f"; links 10.1.2.102:{port}"+
+                                          f'\n')
     else:
-        print("PHP")
+        session2.console.keyboard.put_keys("sudo apt update"+ 
+                                      "; sudo apt install links"+
+                                        "; sudo apt install apache2"+
+                                        "; sudo apt install php php-cgi libapache2-mod-php php-common php-pear php-mbstring"+
+                                        f"; sudo mkdir -p /var/www/{folderName}/html"+
+                                        f"; sudo touch /var/www/{folderName}/html/index.html"+
+                                        f"; sudo echo -e '<html>\n<body>\n<p>HELLO WORLD</p>\n</body>\n</html>' | sudo tee /var/www/{folderName}/html/index.html"+
+                                        f"; sudo chown -R $USER:$USER /var/www/{folderName}/html"+
+                                        f"; sudo chmod -R 755 /var/www/{folderName}"+
+                                        "; sudo nano /etc/apache2/sites-available/000-default.conf"+
+                                        "; sudo nano /etc/apache2/apache2.conf"+
+                                        "; sudo nano /etc/apache2/ports.conf"+
+                                        "; sudo systemctl restart apache2"+
+                                        f"; links 10.1.2.102:{port}"+
+                                      f'\n')
+        
+        session.console.keyboard.put_keys("sudo apt update"+
+                                        "; sudo apt install links"+
+                                        "; sudo apt install php php-cgi libapache2-mod-php php-common php-pear php-mbstring" +
+                                        "; sudo apt install apache2"+
+                                        f"; links 10.1.2.102:{port}"+
+                                          f'\n')
         
 
 
